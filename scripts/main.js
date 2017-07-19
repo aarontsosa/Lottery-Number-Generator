72315e5cf74e0c95dcdce259ad449d7337c3e182
Aaron Sosa <aarontsosa@gmail.com>
2017-07-19 15:11:12 -0400

    added theJS, html front, and some css using bootstrap

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+var winners = {}
+
+function randomNum(stop){
+    num = Math.floor((Math.random() * stop) + 1)
+    return num
+}
+
+function checkNum(stop){
+    num = randomNum(stop)
+    if (num in winners){
+        num = randomNum(stop)
+        return num
+    } else {
+        return num
+    }
+}
+
+function pickNum(stop,len){
+    for(i=0;i<len;i++){
+        num = checkNum(stop);
+        winners[i] = num
+    }
+    return winners
+}
+
+function lotto(stop, len){
+    var lottoNum = " "
+    pickNum(stop,len)
+    for(i=0;i<len;i++){
+        lottoNum += winners[i] + " "
+    }
+    return lottoNum
+}
+
+function printNum(){
+    var top5 = document.querySelector('[data-winner="target"]')
+    var bonus = document.querySelector('[data-bonus="target"]')
+    top5.textContent = "number:" + lotto(69,5)
+    bonus.textContent = "bonus:" + lotto(26,1)
+}
